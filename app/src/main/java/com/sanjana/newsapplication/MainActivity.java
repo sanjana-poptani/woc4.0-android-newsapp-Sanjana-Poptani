93455a19fd1d9d1f5dd840dd0feca70bd99ad65e
sanjana-poptani54 <sanjanapoptani54@gmail.com>
2022-01-22 02:41:24 +0530

    UI second part

--- app/src/main/java/com/sanjana/newsapplication/MainActivity.java
+++ app/src/main/java/com/sanjana/newsapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.sanjana.newsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,90 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button  btnone = findViewById(R.id.button23);
+        Button  btntwo = findViewById(R.id.button24);
+        Button  btnthree = findViewById(R.id.button25);
+        Button  btnfour = findViewById(R.id.button26);
+        Button  btnfive = findViewById(R.id.button27);
+        Button  btnsix = findViewById(R.id.button28);
+        LinearLayout LinearLayout = findViewById(R.id.linearLayout);
+
+
+        btnone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+        btntwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+        btnthree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+        btnfour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+        btnfive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+        btnsix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewScreen newsVerticalLayout = new NewScreen();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout,newsVerticalLayout);
+                transaction.commit();
+            }
+        });
+
+
     }
+
+
+
+
+//        btnone.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            NewsVerticalLayout newsVerticalLayout = new NewsVerticalLayout();
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            transaction.replace(R.id.linearLayout,newsVerticalLayout);
+//        }
+//    });
+
+
 }
---
+++ app/src/main/java/com/sanjana/newsapplication/NewScreen.java
@@ -0,0 +1,27 @@
+package com.sanjana.newsapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewScreen extends Fragment {
+
+    public NewScreen() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_screen, container, false);
+
+        return view;
+    }
+}
